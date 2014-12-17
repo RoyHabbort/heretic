d0634e6b4b55dedf24c9cb41eb7fe371c35e3eb3
RoyHabbort <resq7@mail.ru>
2014-12-17 15:29:05 +0400

    MSG

--- protected/widget/slider/views/_appartament.php
+++
@@ -1,9 +0,0 @@
-<div class="appartament-slider-widget slider-clear">
-    
-    <div class="slider-appartament-initiate" >
-        <div ng-repeat="slider in sliderAppartament" class="slide slide-appartament"><img src="/{{ slider.medium_path }}"></div>
-        
-    </div>
-    
-</div>    
-
